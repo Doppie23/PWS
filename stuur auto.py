6f63eeb2de35fa7e4d071864055807c38f5ca92f
Doppie22 <dvopstal05@gmail.com>
2022-07-08 18:50:21 +0200

    stuur

--- stuur auto.py
+++ stuur auto.py
@@ -4,6 +4,7 @@ from time import sleep
 servo = AngularServo(18, min_pulse_width=0.0008, max_pulse_width=0.0020)
 
 while True:
+    #angle is 45 om  de servo heel te houden... kreeg het anders niet goed werkend
     servo.angle = 45
     sleep(2)
     servo.angle = 0
