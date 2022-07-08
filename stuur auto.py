b9ed2cc4d647b1b13c3731a4f62ea4d790a737ae
Doppie23 <108668353+Doppie23@users.noreply.github.com>
2022-07-08 22:21:05 +0200

    Update stuur auto.py

--- stuur auto.py
+++ stuur auto.py
@@ -1,10 +1,11 @@
 from gpiozero import AngularServo
 from time import sleep
 
+#min en max pulse width moeten miss nog getuned worden
 servo = AngularServo(18, min_pulse_width=0.0008, max_pulse_width=0.0020)
 
 while True:
-    #angle is 45 om  de servo heel te houden... kreeg het anders niet goed werkend
+    #angle is 45 want dat is de angle van de servo
     servo.angle = 45
     sleep(2)
     servo.angle = 0
