03f62a548c0fe60669f94ea7a0f3bd017a863200
Doppie22 <dvopstal05@gmail.com>
2022-07-10 18:51:01 +0200

    hoek aanpassing

--- code/controlsauto.py
+++ code/controlsauto.py
@@ -1,7 +1,7 @@
 from gpiozero import AngularServo
 
 #hoek is nog niet goed of wel geen idee
-servo = AngularServo(18, min_angle=-15, max_angle=20)
+servo = AngularServo(18, min_angle=-35, max_angle=35, min_pulse_width=0.001, max_pulse_width=0.0018)
 
 def Stuurhoek(hoek):
     #code voor hoek
