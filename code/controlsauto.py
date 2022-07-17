d985d12086f6e5432ca2205ff3ff126892bcd000
doppie22 <dvopstal05@gmail.com>
2022-07-17 13:38:35 +0200

    Motor werkt nu redelijk

--- code/controlsauto.py
+++ code/controlsauto.py
@@ -4,7 +4,7 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 factory = PiGPIOFactory()
 
 #hoek is nu wel redelijk (letop min angle mag niet meer dan ong -20)
-servo = AngularServo(18, min_angle=-40, max_angle=27, min_pulse_width=0.0010, max_pulse_width=0.0018, pin_factory=factory)
+servo = AngularServo(17, min_angle=-40, max_angle=27, min_pulse_width=0.0010, max_pulse_width=0.0018, pin_factory=factory)
 
 
 def Stuurhoek(hoek):
@@ -12,4 +12,24 @@ def Stuurhoek(hoek):
     servo.angle = hoek
     print(hoek)
     
-#Stuurhoek(-15)
+Stuurhoek(-15)
+    
+#moet eerst wel setup runnen, staat hieronder
+def Motor(gas):
+    t.ChangeDutyCycle(gas)
+    print(throttle)
+    
+def setup():
+    sleep(1)
+    IO.setmode(IO.BCM)
+    IO.setup(18, IO.OUT)
+    t=IO.PWM(18,100)
+    throttle = 14
+    t.start(throttle)
+    input("wacht op geluid en druk dan op enter")
+    
+def cleanup():
+    IO.setmode(IO.BCM)
+    IO.setup(18, IO.OUT)
+    IO.setup(17, IO.OUT)
+    IO.cleanup()
