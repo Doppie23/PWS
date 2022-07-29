a15d6d4473b4f0e9a042564b30155871ee4ab8bf
Doppie22 <dvopstal05@gmail.com>
2022-07-29 14:36:07 +0200

    werkt soortvan lijne combi

--- code/lijn volg test/lijnherkennen.py
+++ code/lijn volg test/lijnherkennen.py
@@ -7,8 +7,8 @@ def hsvkleur(img):
     return hsv
 
 def paarsalleen(hsvimg):
-    lower_blue = np.array([60, 40, 40])
-    upper_blue = np.array([150, 255, 255])
+    lower_blue = np.array([100, 5, 81])
+    upper_blue = np.array([180, 255, 255])
     mask = cv2.inRange(hsvimg, lower_blue, upper_blue)
     return mask
 
@@ -26,10 +26,49 @@ def lijnendetect(canny):
     # getal moeten miss nog aangepast is trial en error
     rho = 1
     angle = np.pi / 180
-    min_threshold = 15  # minimum stemmen om te tellen als lijn
+    min_threshold = 20  # minimum stemmen om te tellen als lijn
     lijnen = cv2.HoughLinesP(canny, rho, angle, min_threshold, np.array([]), 
-                                minLineLength=8, maxLineGap=10)
+                                minLineLength=60, maxLineGap=60)
     return lijnen
+
+def average(image, lines):
+    left = []
+    right = []
+    for line in lines:
+        print(line)
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        y_int = parameters[1]
+        if slope < 0:
+            left.append((slope, y_int))
+        else:
+            right.append((slope, y_int))
+    right_avg = np.average(right, axis=0)
+    left_avg = np.average(left, axis=0)
+    left_line = make_points(image, left_avg)
+    right_line = make_points(image, right_avg)
+    return np.array([left_line, right_line])
+
+def make_points(frame, line): 
+    height, width, _ = frame.shape
+    slope, intercept = line
+    y1 = height  # bottom of the frame
+    y2 = int(y1 * 1 / 2)  # make points from middle of the frame down
+
+    # bound the coordinates within the frame
+    x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
+    x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+    return [[x1, y1, x2, y2]]
+
+def display_lines(frame, lines, line_color=(0, 255, 0), line_width=2):
+    line_image = np.zeros_like(frame)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
+    line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    return line_image
 
 
 camera = 1
@@ -42,7 +81,9 @@ while True:
     canny = cannyedge(mask)
     crop = cropimg(canny)
     lijnen = lijnendetect(crop)
-    print(lijnen)
+    averaged_lines = average(frame, lijnen)
+    lane_lines_image = display_lines(frame, averaged_lines)
+    
 
     # alle imshow dingen:
 
@@ -51,10 +92,12 @@ while True:
         for line in lijnen:
             x1, y1, x2, y2 = line[0]
             cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+    cv2.imshow("hsv", hsvimg)
     cv2.imshow("hough", frame)
-    # cv2.imshow("mask", mask)
+    cv2.imshow("mask", mask)
     cv2.imshow("crop", crop)
     cv2.imshow("canny", canny)
+    cv2.imshow("lane lines", lane_lines_image)
 
 
 
