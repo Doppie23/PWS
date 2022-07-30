a1fb0516c187cdcbb72e5f04ab1d9e583d6f3a92
Doppie22 <dvopstal05@gmail.com>
2022-07-30 14:51:56 +0200

    import moet zo

--- code/lijn volg test/test import.py
+++ code/lijn volg test/test import.py
@@ -2,8 +2,10 @@ from lijnherkennen import lijn_volgen
 import cv2
 
 while True:
-    lijn_volgen()
-    
+    hoek = lijn_volgen()
+    print(hoek)
+
+
     key = cv2.waitKey(1)
     if key == 27:
         break
