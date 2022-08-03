56ac92201d8e7c73c22d61910e4c271ca2c59036
Doppie22 <dvopstal05@gmail.com>
2022-08-03 23:00:58 +0200

    testen van code voor minder gas bij grote hoek

--- code/lijn volg test/test import.py
+++ code/lijn volg test/test import.py
@@ -1,13 +1,28 @@
-from lijnherkennen import lijn_volger
+from lijnherkennenorig import lijn_volger
 import cv2
 
 
 hoek = lijn_volger(0) #is om beginhoek op 0 te zetten moet dus wel eerst nog gedaan worden
 
+gas = 14.9
 
 while True:
     stuurhoek = hoek.stabhoek()
-    print(stuurhoek)
+
+    if stuurhoek < -20:
+        stuurhoek = -20
+        gas = 14.7
+    elif stuurhoek > 27:
+        stuurhoek = 27
+        gas = 14.7
+    elif gas == 14.7 and stuurhoek < 27 and stuurhoek > -20:    # om gas weer terug te zetten als de hoek weer in servo range zit
+        gas = 14.9
+
+    print("hoek:", stuurhoek, "gas:", gas)
+    
+
+
+
     key = cv2.waitKey(1)
     if key == 27:
         break
