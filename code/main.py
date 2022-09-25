707bb3bad4f3db804b42bca35e071469752aa274
Doppie22 <dvopstal05@gmail.com>
2022-09-25 11:14:32 +0200

    update voor nieuwe motor

--- code/main.py
+++ code/main.py
@@ -9,18 +9,17 @@ maxstuurhoek = 29
 
 # initialiseren van auto
 print("auto aan het initialiseren")
-sleep(1)
 IO.setmode(IO.BCM)
 IO.setup(18, IO.OUT)
 t=IO.PWM(18,100)
 throttle = 14
 t.start(throttle)
-input("wacht op geluid van esc druk dan op enter")
+input("licht op esc aan?")
 
 ca.Stuurhoek(0)
 hoek = lijn_volger(0) #0 is de begin hoek van de servo
 input("klaar om te gaan druk op enter")
-gas = 14.9
+gas = 20
 t.ChangeDutyCycle(gas)
 
 while True:
@@ -32,16 +31,16 @@ while True:
     #zodat de servo niet te ver gaat en als de hoek wel zo groot is gaat de motor langzamer
     if stuurhoek < minstuurhoek:
         stuurhoek = minstuurhoek
-        if gas != 14.7:
-            gas = 14.7
+        if gas != 16:
+            gas = 16
             t.ChangeDutyCycle(gas)
     elif stuurhoek > maxstuurhoek:
         stuurhoek = maxstuurhoek
-        if gas != 14.7:
-            gas = 14.7
+        if gas != 16:
+            gas = 16
             t.ChangeDutyCycle(gas)
-    elif gas == 14.7 and stuurhoek < maxstuurhoek and stuurhoek > minstuurhoek:    # om gas weer terug te zetten als de hoek weer in servo range zit
-        gas = 14.9
+    elif gas == 16 and stuurhoek < maxstuurhoek and stuurhoek > minstuurhoek:    # om gas weer terug te zetten als de hoek weer in servo range zit
+        gas = 20
         t.ChangeDutyCycle(gas)
     
     ca.Stuurhoek(stuurhoek)
