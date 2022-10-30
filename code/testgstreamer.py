d615e1dd094f0b3792946570a18eb1f691e21d96
Doppie22 <dvopstal05@gmail.com>
2022-10-30 12:48:18 +0100

    3

--- code/testgstreamer.py
+++ code/testgstreamer.py
@@ -79,6 +79,7 @@ def main():
     fps_counter = avg_fps_counter(30)
 
     def user_callback(input_tensor, src_size, inference_box):
+      gas=20
       nonlocal fps_counter
       start_time = time.monotonic()
       run_inference(interpreter, input_tensor)
