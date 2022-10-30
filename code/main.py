c6bc31d47c05c804a27e922edbd157981e5f91e7
Doppie22 <dvopstal05@gmail.com>
2022-10-30 14:00:51 +0100

    fresh frame

--- code/main.py
+++ code/main.py
@@ -4,6 +4,7 @@ import controlsauto as ca
 from time import sleep
 import RPi.GPIO as IO
 import numpy as np
+import threading
 import math
 
 from pycoral.adapters.common import input_size
@@ -37,6 +38,7 @@ def main():
     input("licht op esc aan?")
 
     cap = cv2.VideoCapture(0)
+    fresh = FreshestFrame(cap)
 
     ca.Stuurhoek(0)
     hoek = lijn_volger() #0 is de begin hoek van de servo
@@ -45,10 +47,10 @@ def main():
     t.ChangeDutyCycle(gas)
 
     while True:
-        ret, frame = cap.read()
-        if not ret:
+        cnt,img = fresh.read(seqnumber=cnt+1)
+        if not cnt:
             break
-        cv2_im = frame
+        cv2_im = img
 
         """
         object herkenning
@@ -97,8 +99,9 @@ def main():
         key = cv2.waitKey(1)
         if key == 27:
             break
+
+    fresh.release()
     ca.cleanup()
-    cap.release()
     cv2.destroyAllWindows()
 
 def append_objs_to_img(cv2_im, inference_size, objs, labels):
@@ -161,6 +164,76 @@ def auto_ziet_bord(objs, labels, gas):
         stoppen = False
 
     return stoppen, gas
+
+class FreshestFrame(threading.Thread):
+	def __init__(self, capture, name='FreshestFrame'):
+		self.capture = capture
+		assert self.capture.isOpened()
+
+		# this lets the read() method block until there's a new frame
+		self.cond = threading.Condition()
+
+		# this allows us to stop the thread gracefully
+		self.running = False
+
+		# keeping the newest frame around
+		self.frame = None
+
+		# passing a sequence number allows read() to NOT block
+		# if the currently available one is exactly the one you ask for
+		self.latestnum = 0
+
+		# this is just for demo purposes		
+		self.callback = None
+		
+		super().__init__(name=name)
+		self.start()
+
+	def start(self):
+		self.running = True
+		super().start()
+
+	def release(self, timeout=None):
+		self.running = False
+		self.join(timeout=timeout)
+		self.capture.release()
+
+	def run(self):
+		counter = 0
+		while self.running:
+			# block for fresh frame
+			(rv, img) = self.capture.read()
+			assert rv
+			counter += 1
+
+			# publish the frame
+			with self.cond: # lock the condition for this operation
+				self.frame = img if rv else None
+				self.latestnum = counter
+				self.cond.notify_all()
+
+			if self.callback:
+				self.callback(img)
+
+	def read(self, wait=True, seqnumber=None, timeout=None):
+		# with no arguments (wait=True), it always blocks for a fresh frame
+		# with wait=False it returns the current frame immediately (polling)
+		# with a seqnumber, it blocks until that frame is available (or no wait at all)
+		# with timeout argument, may return an earlier frame;
+		#   may even be (0,None) if nothing received yet
+
+		with self.cond:
+			if wait:
+				if seqnumber is None:
+					seqnumber = self.latestnum+1
+				if seqnumber < 1:
+					seqnumber = 1
+				
+				rv = self.cond.wait_for(lambda: self.latestnum >= seqnumber, timeout=timeout)
+				if not rv:
+					return (self.latestnum, self.frame)
+
+			return (self.latestnum, self.frame)
 
 if __name__ == '__main__':
     main()
