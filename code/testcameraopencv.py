4b9a5ea0c73f8c66191d510e592f8c0bfc71f253
Doppie22 <dvopstal05@gmail.com>
2022-12-06 14:06:10 +0100

    werkt weer

--- code/testcameraopencv.py
+++ code/testcameraopencv.py
@@ -4,8 +4,11 @@ is om te testen of alle values in lijnherkennen.py goed zijn
 from lijnherkennen import lijn_volgen
 import cv2
 
+cap = cv2.VideoCapture(2)
+
 while True:
-    lijn_volgen() #alle imshows uncommenten door in deze functie (progamertip selecteer de regels en druk dan ctrl + /)
+    _, img = cap.read()
+    lijn_volgen(img) #alle imshows uncommenten door in deze functie (progamertip selecteer de regels en druk dan ctrl + /)
     key = cv2.waitKey(1)
     if key == 27:
         break
